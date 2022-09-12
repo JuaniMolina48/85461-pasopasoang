7ef12a32f4c66a5b110e476dbc020c2ec6fc8ed8
JuaniMolina48 <95692195+JuaniMolina48@users.noreply.github.com>
2022-09-12 17:50:14 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular Hassan ' + VERSION.major;
+  saludo = 'hassan como estas?';
+}
